ea1c2e0071dfa53aabdd3189bc89a4c016c1df69
Saba Ansari <sabaansari7110@gmail.com>
2026-02-01 18:48:13 +0530

    Add files via upload

---
+++ subjeact-marks-dictionary.py
@@ -0,0 +1,12 @@
+
+marks={}
+x=int(input("enter phy:"))
+marks.update({"phy": x})
+
+x=int(input("enter chem:"))
+marks.update({"chem": x})
+
+x= int(input("enter bio:"))
+marks.update({"bio": x})
+
+print(marks)
